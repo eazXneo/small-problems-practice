d2154361ec5b2f7065b9a753e1c89921eccee355
leaa2019 <leaa.asw2019@gmail.com>
2022-05-26 16:53:13 +0200

    more of t1a

---
+++ T1b_longest_substring.py
@@ -0,0 +1,26 @@
+# _source_ https://github.com/cl2333/Grokking-the-Coding-Interview-Patterns-for-Coding-Questions/blob/master/1.%20Pattern%20Sliding%20Window/Maximum%20Sum%20Subarray%20of%20Size%20K%20(easy).py
+# _Topic_ @1 Pattern: Sliding Window
+# _Q_ @1. b)
+# _Difficulty_ medium
+
+
+# Problem Statement:
+"""
+Given a string, find the length of the longest substring in it with no more than K distinct characters.
+Example 1:
+Input: String="araaci", K=2
+Output: 4
+Explanation: The longest substring with no more than '2' distinct characters is "araa".
+Example 2:
+Input: String="araaci", K=1
+Output: 2
+Explanation: The longest substring with no more than '1' distinct characters is "aa".
+Example 3:
+Input: String="cbbebi", K=3
+Output: 5
+Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
+"""
+
+# my solution
+def longest_substring_with_k_distinct(str, k):
+	
