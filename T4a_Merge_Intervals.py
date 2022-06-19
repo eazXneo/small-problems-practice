c5d65bd9bbb43a9886f67cba30917baceac6192e
leaa2019 <leaa.asw2019@gmail.com>
2022-06-19 19:34:03 +0200

    incoming 4.a) and 5.a)

---
+++ T4a_Merge_Intervals.py
@@ -0,0 +1,4 @@
+# https://github.com/cl2333/Grokking-the-Coding-Interview-Patterns-for-Coding-Questions/blob/master/4.%20Pattern%20Merge%20Intervals/Merge%20Intervals%20(medium).py
+# _Topic_ @5 Pattern: Merge Intervals
+# _Q_ @4. a)
+# _Difficulty_ medium
