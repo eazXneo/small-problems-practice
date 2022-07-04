5fbdb4d08131bf7145fe208aed963935078ad9a0
leaa2019 <leaa.asw2019@gmail.com>
2022-07-04 16:03:57 +0200

    8.a) my solution with stack runs, but has incorrect sum :(

--- T8a_Binary_Tree_Path_Sum.py
+++ T8a_Binary_Tree_Path_Sum.py
@@ -11,31 +11,41 @@ find if the tree has a path from root-to-leaf such that the sum of all the
 node values of that path equals ‘S’.
 """
 
+
 # from answer
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 # My solution:
 # worst case binary tree is a LL
 # use DFS to find all paths from root to leaf
 # use stack (or recursion I guess)
+# CURRENTLY ABANDONING STACK VERSION OF DFS.
+# would need a "visited" array or something to keep track of nodes already
+# acessed when backtracking.
 def MY_has_path(root, sum):  # from Q I guess it returns boolean. # Q literally says 'S'
     stack = [root]
+    visited = []
     current_sum = 0
     while len(stack) != 0:
         current_node = stack.pop()
-        # update current_sum:
-        current_sum += current_node.val
+        if current_node not in visited:
+            visited.append(current_node)
+            # update current_sum:
+            current_sum += current_node.val
 
-        if current_sum == sum:
-            return True
-        if current_node.left is not None:
-            stack.append(current_node.left)
-        elif current_node.right is not None:
-            stack.append(current_node.right)
-        else:  # trying to backtrack with the total sum
+            if current_sum == sum:
+                return True
+            if current_node.right is not None:
+                stack.append(current_node.right)
+
+            if current_node.left is not None:
+                stack.append(current_node.left)
+        else:
+            # trying to backtrack with the total sum
             current_sum -= current_node.val
     return False
 # deque vs list rep...?
@@ -44,8 +54,10 @@ def MY_has_path(root, sum):  # from Q I guess it returns boolean. # Q literally 
 # base case as well? if root is None, return False
 # keep calling has_path_rec(root, sum-curr.val) for left AND
 # right children
+# improvement in this case, if current_sum > sum, then abort that path entirely, saving time?
 
 ## ANSWER
+# drew out the recursion tree and I think it makes sense, ish.
 def has_path(root, sum):  # neat, but also recursion, which hurts brain :(
     if root is None:
         return False
@@ -58,6 +70,7 @@ def has_path(root, sum):  # neat, but also recursion, which hurts brain :(
     # return true if any of the two recursive call return true
     return has_path(root.left, sum - root.val) or has_path(root.right, sum - root.val)
 
+
 """ Asymptotics:
 For sols: O(n) <- goes down every path possible in the tree once and so accesses 'n' nodes
 For mine: O(n) <- same reasoning as above
@@ -67,13 +80,18 @@ Space complexity: O(n) <- because worst-case the tree is a linked list :(
 
 # Additional code
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  print("Tree has path: " + str(has_path(root, 23)))
-  print("Tree has path: " + str(has_path(root, 16)))
+    print("My answer:")
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree has path: " + str(MY_has_path(root, 23)))
+    print("Tree has path: " + str(MY_has_path(root, 16)))
+
+    print("Solutions:")
+    print("Tree has path: " + str(has_path(root, 23)))
+    print("Tree has path: " + str(has_path(root, 16)))
 
 main()
